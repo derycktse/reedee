17a3ed5e037650882ecdf944080f801ce6045789
Derycktse <285205451@qq.com>
2017-06-26 20:59:40 +0800

    change

--- app/components/search.jsx
+++ app/components/search.jsx
@@ -4,7 +4,9 @@ class Search extends React.Component{
   render(){
     return (
       <div>
+        <button></button>
         <input type="text"/>
+        <button></button>
       </div>
     )
   }
