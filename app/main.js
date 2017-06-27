337a3a9db9d8d51b33324f42e892988005268b53
Derycktse <285205451@qq.com>
2017-06-27 20:54:08 +0800

    modify

--- app/main.js
+++ app/main.js
@@ -1,8 +1,7 @@
 
 import ReactDOM from 'react-dom'
- 
+import React from 'react' 
 import List from './components/list.jsx'
-import react from 'react'
 
 ReactDOM.render(
   <List />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  devtool: '#cheap-module-eval-source-map',
   entry: './app/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -19,8 +20,12 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, ''),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'index.html'
+  })]
 }
