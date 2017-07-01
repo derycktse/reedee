0db60903412ab23c69b813c4fbf3183012b3f060
Derycktse <285205451@qq.com>
2017-07-01 17:49:05 +0800

    add api

---
+++ config/api.js
@@ -0,0 +1,84 @@
+const api = {
+  "token": {
+    api: "/reader/api/0/token",
+    type: "1"
+  },
+  "save-user-pref": {
+    api: "/reader/api/0/save-user-pref",
+    type: "2"
+  },
+  "user-info": {
+    api: "/reader/api/0/user-info",
+    type: "1"
+  },
+  "preference/list": {
+    api: "/reader/api/0/preference/list",
+    type: "1"
+  },
+  "stream/list": {
+    api: "/reader/api/0/preference/stream/list",
+    type: "1"
+  },
+  "set": {
+    api: "/reader/api/0/preference/stream/set",
+    type: "2"
+  },
+  "tag/list": {
+    api: "/reader/api/0/tag/list",
+    type: "1"
+  },
+  "subscription/list": {
+    api: "/reader/api/0/subscription/list",
+    type: "1"
+  },
+  "mark-all-as-read": {
+    api: "/reader/api/0/mark-all-as-read",
+    type: "2"
+  },
+  "mark-all-as-read-undo": {
+    api: "/reader/api/0/mark-all-as-read-undo",
+    type: "2"
+  },
+  "quickadd": {
+    api: "/reader/api/0/subscription/quickadd",
+    type: "2"
+  },
+  "edit": {
+    api: "/reader/api/0/subscription/edit",
+    type: "2"
+  },
+  "unread-count": {
+    api: "/reader/api/0/unread-count",
+    type: "1"
+  },
+  "edit-tag": {
+    api: "/reader/api/0/edit-tag",
+    type: "2"
+  },
+  "rename-tag": {
+    api: "/reader/api/0/rename-tag",
+    type: "2"
+  },
+  "disable-tag": {
+    api: "/reader/api/0/disable-tag",
+    type: "2"
+  },
+  "atom": {
+    api: "/reader/atom",
+    type: "1"
+  },
+  "stream/contents": {
+    api: "/reader/api/0/stream/contents",
+    type: "1"
+  },
+  "items/contents": {
+    api: "/reader/api/0/stream/items/contents",
+    type: "1"
+  },
+  "ids": {
+    api: "/reader/api/0/stream/items/ids",
+    type: "1"
+  },
+}
+
+export default api
