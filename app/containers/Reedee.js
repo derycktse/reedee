4bc62a5eca5605aa90765c442eddc8e78786244e
Derycktse <285205451@qq.com>
2017-07-10 20:40:05 +0800

    add auth

--- app/containers/Reedee.js
+++ app/containers/Reedee.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import SubscriptionFolder from '../components/Subscription-folder'
 import SubscriptionMenu from '../components/Subscription-menu'
 import styles from './Reedee.css'
+import * as cookie from '../../config/cookie'
+//write cookie
+document.cookie = cookie
 export default class Reedee extends Component {
   render() {
     return (
