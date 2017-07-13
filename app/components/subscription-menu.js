db9fe57a48c559a06ec91de2b57b9a99b2cc1e64
Derycktse <285205451@qq.com>
2017-07-13 20:41:30 +0800

    add fetch data test

--- app/components/subscription-menu.js
+++ app/components/subscription-menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from './Subscription-menu.css';
+import { Link } from 'react-router-dom';
 
 const SubscriptionItem = ({
   subscriptionName = 'subscriptionName',
@@ -19,25 +20,44 @@ const SubscriptionItem = ({
 const FilterBox = () => {
   return (
     <div>
+      <Link to="/">to Home</Link>
       <div>filter panel</div>
     </div>
   )
 }
 
 export default class SubscriptionMenu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
   componentDidMount() {
+    fetch('http://localhost:3000/subscription/list').then(res => {
+      return res.json()
+    }).then(data => {
+      let subscriptions = data.subscriptions
 
+      console.log(subscriptions)
+      this.setState({
+        subscriptions
+      })
+    })
   }
   render() {
-    let props = this.props
-    let subscriptionList = [1, 2, 3, 5]
+    let state = this.state
+    let subscriptionList = state.subscriptions || []
     return (
       <div id="subscription-menu">
         <ul className="subscpt-set">
           {
             subscriptionList.map((sub, idx) => {
+
+              let title = sub.title
+              let obj = {
+                title
+              }
               return (
-                <SubscriptionItem key={idx} />
+                <SubscriptionItem key={idx} {...obj} />
               )
             })
           }
