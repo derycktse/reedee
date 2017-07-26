b026645c1c9e40ed45dcee580d78852a153e6356
Derycktse <285205451@qq.com>
2017-07-26 16:44:19 +0800

    remove useless

--- app/reducers/reedee.js
+++ app/reducers/reedee.js
@@ -1,14 +1,20 @@
+import { combineReducers } from 'redux';
 
-// let subscriptionsSerialize = '', tagListSerialize = ''
-// try {
-//   subscriptionsSerialize = localStorage.getItem('subscriptions')
-//   tagListSerialize = localStorage.getItem('tag-list')
-// } catch (e) {
-//   // log
-// }
-// const subscriptions = (JSON.parse(subscriptionsSerialize) || {}).subscriptions || []
-// const tags = (JSON.parse(tagListSerialize) || {}).tags || []
-
+function subscriptionList(state = getInitState('subscription-list'), type) {
+  return state
+}
+function tagList(state = getInitState('tag-list'), type) {
+  return state
+}
+function unreadCount(state = getInitState('unread-count'), type) {
+  return state
+}
+function panelController(state = getInitState('subscription-panel-status-controller'), type) {
+  return state
+}
+function streamContent(state = getInitState('stream/contents'), type) {
+  return state
+}
 
 function getInitState(...names) {
   const obj = {}
@@ -19,31 +25,13 @@ function getInitState(...names) {
 }
 
 const initList = ['subscription-list', 'tag-list', 'unread-count', 'subscription-panel-status-controller', 'stream/contents']
+
+
 const initState = getInitState(...initList)
 console.log('init state:')
 console.log(initState)
 export default function reedee(state = initState, action) {
   switch (action.type) {
-    case 'FETCH_SUBSCRIPTION_LIST':
-      return {
-        ...state
-      }
-    case 'SYNC':
-      console.log('sync')
-      console.log(action)
-      return {
-        ...state
-      }
-    case 'UPDATE_TAG_LIST':
-      return {
-        ...state,
-        tags: action.payload
-      }
-    case 'UPDATE_SUBSCRIPTION_LIST':
-      return {
-        ...state,
-        subscriptions: action.payload
-      }
     case 'UPDATE_DATA':
       return {
         ...state,
