452a0661e7b52c3add7d7221eb6fdaba075083ad
Derycktse <285205451@qq.com>
2017-08-12 13:27:18 +0800

    add databases

--- app/db/db.js
+++ app/db/db.js
@@ -1,12 +1,13 @@
+import path from 'path'
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./database.sqlite3');
+var db = new sqlite3.Database(path.resolve(__dirname, '../../databases/database.sqlite3'));
 
 db.serialize(function () {
   db.run("CREATE TABLE lorem (info TEXT)");
 
   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
   for (var i = 0; i < 10; i++) {
-    stmt.run("Ipsum " + i);
+    stmt.run("Ipsum " + i + 'deryck');
   }
   stmt.finalize();
 
